390664053670f29317fbdd20e1fe0c29049ce2cb
Piyush Gupta <180550158+Piyush495@users.noreply.github.com>
2026-08-03 12:34:30 +0530

    add login and signup page

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { useState } from "react"
+
+export default function Login(){
+
+    const [formData,setFormData]=useState({name:"",email:"",password:""})
+
+    const handleSubmit=()=>{
+        
+    }
+
+    return (
+        <div className=""></div>
+    )
+    
+}
